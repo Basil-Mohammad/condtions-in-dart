eb55ca95ac0422ef285dd365b63a95cc1a635022
Basil-Mohammad <134337008+Basil-Mohammad@users.noreply.github.com>
2024-07-28 19:47:06 +0300

    Add files via upload

---
+++ p3.dart
@@ -0,0 +1,67 @@
+import 'dart:io';
+void main(){
+  // condtions in dart
+  // if condtion example
+  /*
+  print("Enter your Age please:");
+  var Age = int.parse(stdin.readLineSync()!);
+  //var Age =20;
+  if(Age > 18){
+    print("you can applay for loan");
+    }
+    else{
+      print("come back when you reach 18");
+    }
+  */
+
+  // switch example
+
+/*
+   print("Enter Day Number please:");
+  var dayOfWeek= int.parse(stdin.readLineSync()!);
+  //var dayOfWeek =5;
+  switch(dayOfWeek){
+    case 0:
+    case 1:
+    print("Day is Sunday");
+    break;
+    case 2:
+    print("Day is Monday");
+    break;
+    case 3:
+    print("Day is Tusday");
+    break;
+    case 4:
+    print("Day is Wensday");
+    break;
+    case 5:
+    print("Day is Thersday");
+    break;
+    case 6:
+    print("Day is Friday");
+    break;
+    case 7:
+    print("Day is Saturday");
+    break;
+    default:
+    print("Invaled Week Day");
+    break;
+  }
+
+*/
+
+// Ternary Operator Examples
+ /*
+  int num1 = 10;
+  int num2 = 15;
+  int max =(num1 > num2)? num1 : num2;
+  print("The greatest number is $max");
+  */
+
+// Another Example
+  /*
+  int a =20;
+  String output = (a>=13 && a<=19)?"Teenager":"Not Teenager";
+  print (output);
+  */
+}
